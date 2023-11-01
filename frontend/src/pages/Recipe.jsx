2d2a0195f196c6beea969ca5587729860ddc0bc0
henryrossi <henry.rossi219@gmail.com>
2023-11-01 12:57:27 -0700

    removed old code that was in a comment

--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -80,61 +80,6 @@ function Recipe() {
             </div>
           </div>
         </>
-
-        // <article className="recipe">
-        //   <h1>{data.getRecipeByName.name}</h1>
-        //   {localStorage.getItem("token") && (
-        //     <Link to={"/edit/" + recipeName}>Edit</Link>
-        //   )}
-        //   <section className="info">
-        //     <p className="servingsAndTime">
-        //       {data.getRecipeByName.servings} servings |{"  "}
-        //       {data.getRecipeByName.time} minutes
-        //     </p>
-        //     <h2>{data.getRecipeByName.description}</h2>
-        //     <ul>
-        //       {data.getRecipeByName.category.map((tag) => (
-        //         <li className="tag" key={tag.name}>
-        //           {tag.name.toLowerCase()}
-        //         </li>
-        //       ))}
-        //     </ul>
-        //     {data.getRecipeByName.base64picture ? (
-        //       <img
-        //         className="photo"
-        //         src={
-        //           "data:image/jpeg;charset=utf-8;base64," +
-        //           atob(data.getRecipeByName.base64picture)
-        //         }
-        //         alt=""
-        //       />
-        //     ) : (
-        //       <div className="noPicture">
-        //         <p className="message">image unavailable</p>
-        //       </div>
-        //     )}
-        //   </section>
-        //   <div className="ingredients">
-        //     <p>Ingredients</p>
-        //     <ul>
-        //       {data.getRecipeByName.ingredientList.map((obj, index) => (
-        //         <li className="ingredient" key={index}>
-        //           {obj.measurement} {obj.ingredient.name}
-        //         </li>
-        //       ))}
-        //     </ul>
-        //   </div>
-        //   <div className="instructions">
-        //     <p>Instructions</p>
-        //     <ul>
-        //       {data.getRecipeByName.instructions
-        //         .split("\r")
-        //         .map((instruction) => (
-        //           <li key={instruction}>{instruction}</li>
-        //         ))}
-        //     </ul>
-        //   </div>
-        // </article>
       )}
       <Footer />
     </div>
