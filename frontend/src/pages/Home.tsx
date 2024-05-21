bd50332fd049dff754347b793d3211d6282bc412
Henry Rossi <henry.rossi219@gmail.com>
2024-05-21 10:32:24 -0700

    home page css refactored

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { IconPencilPlus, IconLogout } from "@tabler/icons-react";
 import "./Home.css";
 import "@fontsource/jua";
+import Couple from "../images/hotcouple.png";
+import Kitchen from "../images/kitchen.png";
 
 function Home() {
   const [loggedIn, setAuthorization] = React.useState(localStorage.getItem("token"));
@@ -15,44 +17,53 @@ function Home() {
 
   return (
     <>
-      <hgroup className="hgroup">
-        <h1 className="title">mescolare!</h1>
-        <p className="desc">
+      <div className="main-container-home">
+        <h1 className="text-6xl jua">mescolare!</h1>
+        <p className="red text-xl">
           a collaborative project between two people who love food
         </p>
-      </hgroup>
-      {loggedIn && (
+        <img
+        className="couple-img-home"
+          src={Couple}
+          alt=""
+        />
+      </div>
+      {/* move this to navbar */}
+      {/* {loggedIn && (
         <div>
-          <Link to="/create" className="createLink">
+          <Link to="/create">
             <IconPencilPlus />
           </Link>
-          <button onClick={handleLogout} className="logOutButton">
+          <button onClick={handleLogout}>
             <IconLogout />
           </button>
         </div>
-      )}
-      <img
-        className="home-drawing"
-        src={require("../images/hotcouple.png")}
-        alt=""
-      />
-      <article className="article">
-        <img
-          className="article-img"
-          src={require("../images/kitchen.png")}
-          alt=""
-        />
-        <h2 className="article-title">
-          Our homebase for all of our favorite recipes we love to make with
-          eachother.
-        </h2>
-        <p className="article-text">
-          As you journey through our kitchen we hope these recipes bring as much
-          joy to you as they do to us. We welcome you to explore our website,
-          discover whole new tastes and cuisines, and let the cooking adventures
-          begin!
-        </p>
-      </article>
+      )} */}
+      <div className="relative-positioned-home">
+        <div className="bg-blue background-home">
+          <div className="main-container-home">
+            <div className="grid-home">
+              <div className="grid-text-home">
+                <h2 className="text-4xl yellow">
+                  Our homebase for all of our favorite recipes we love to make with
+                  eachother.
+                </h2>
+                <p className="white text-xl">
+                  As you journey through our kitchen we hope these recipes bring as much
+                  joy to you as they do to us. We welcome you to explore our website,
+                  discover whole new tastes and cuisines, and let the cooking adventures
+                  begin!
+                </p>
+              </div>
+              <img
+                className="grid-image-home"
+                src={Kitchen}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
