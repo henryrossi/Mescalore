3528dff084ad5f6e6265dddc934033d29f2f1edf
Henry Rossi <henry.rossi219@gmail.com>
2024-05-27 13:45:25 -0700

    recipe list css redone

--- frontend/src/components/RecipeList.tsx
+++ frontend/src/components/RecipeList.tsx
@@ -5,22 +5,25 @@ import "./RecipeList.css";
 
 export default function RecipeList({ recipes } : { recipes: RecipeData[] }) {
   return (
-    <div className="recipes-container">
+    <section className="main-container-recipe-list">
       <ul>
         {recipes &&
           recipes.map((recipe) => (
-            <li className="card" key={recipe.name}>
-              <Link to={"/" + recipe.name}>
+            <li className="border-black hover-blue-drop-shadow" key={recipe.name}>
+              <Link 
+                className="link-recipe-list"
+                to={"/" + recipe.name}
+              >
                 {recipe.imageURL ? 
                 <img src={recipe.imageURL} alt="" /> : 
                 <div className="noPicture">
                   <p className="message">image unavailable</p>
                 </div>}
-                <p>{recipe.name}</p>
+                <p className="jua black text-xl">{recipe.name}</p>
               </Link>
             </li>
           ))}
       </ul>
-    </div>
+    </section>
   );
 }
