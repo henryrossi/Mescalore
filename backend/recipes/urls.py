61d03317812ef95adf130dff20374b29a5a80c94
Henry Rossi <henry.rossi219@gmail.com>
2024-11-20 15:17:20 -0800

    removed total recipes api endpoint

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from recipes.views import discover, favorites, recipe, search, total
+from recipes.views import discover, favorites, recipe, search
 
 urlpatterns = [
-    path("total", total.Total.as_view()),
     path("discover", discover.Discover.as_view()),
     path("search", search.SearchRecipes.as_view()),
     path("favorites", csrf_exempt(favorites.Favorites.as_view())),
--- backend/recipes/views/total.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-from django.views import View
-from recipes.models import Recipe
-
-
-class Total(View):
-    def get(self, request):
-        return JsonResponse({"data": Recipe.objects.count()})
